b08c3153d0e77c249ecc1f3737d9d2ea30837dee
Avani Halve <avani.halve@gmail.com>
2022-04-19 10:23:13 +0530

    make it better

--- app/mailers/seatrequest_mailer.rb
+++ app/mailers/seatrequest_mailer.rb
@@ -3,6 +3,7 @@ class SeatrequestMailer < ApplicationMailer
   	@employee = params[:employee]
   	@seat = params[:seat]
   	@name = params[:name]
+  	@reason = params[:reason]
   	mail(from: @employee, to: "avihalve@gmail.com", subject: 'Requested for New Seat')
   end
 end
